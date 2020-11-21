89cdeee84023766b0cab5aaf218c9b82a2d5f96c
admin <admin@USCS-Mac285.local>
2020-11-21 15:17:43 -0800

    prev value stored

--- projects/lpc40xx_freertos/l4_io/joystick/joystick.c
+++ projects/lpc40xx_freertos/l4_io/joystick/joystick.c
@@ -18,13 +18,21 @@ void joystick__initialize(gpio_s joystick__x_axis, gpio_s joystick__y_axis, gpio
 
 joystick__values_s joystick__get_value(void) {
   joystick__values_s joystick_values = {0, 0};
+  static joystick__values_s prev_val = {0.0};
+  static joystick__values_s new_val = {0, 0};
+
+  prev_val = new_val;
   LPC_IOCON->P0_25 &= ~(0x98);
   joystick_values.x = adc__get_adc_value(ADC__CHANNEL_2);
-  joystick_values.x = map(joystick_values.x, 0, 4096, -32, 32);
+  joystick_values.x = map(joystick_values.x, 0, 4096, -64, 0);
   LPC_IOCON->P1_30 &= ~(0x98);
   joystick_values.y = adc__get_adc_value(ADC__CHANNEL_4);
-  joystick_values.y = map(joystick_values.y, 0, 4096, -32, 32);
-  return joystick_values;
+  joystick_values.y = map(joystick_values.y, 0, 4096, 0, 64);
+  if ((joystick_values.x - prev_val.x > 0) || (joystick_values.y - prev_val.y > 0)) {
+    prev_val = new_val;
+    return new_val = joystick_values;
+  } else
+    return prev_val;
 }
 
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
